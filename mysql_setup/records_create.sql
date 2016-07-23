4c57c23e2ee313d7a1631730ca056c527bf969b1
Dan Douthit <danieldouthit@gmail.com>
2016-07-23 15:51:32 -0600

    recommit fixed database setup script after reset

--- mysql_setup/records_create.sql
+++ mysql_setup/records_create.sql
@@ -2,7 +2,7 @@ create table albums
 ( album_id int unsigned auto_increment primary key not null, 
   album_name char(255) not null,
   artist_id int unsigned not null,
-  release_year char(4) not null
+  release_year int unsigned not null
 );
 
 
@@ -20,16 +20,17 @@ create table songs
 );
 
 insert into artists (artist_name, genre) values ('The Killers', 'Rock');
-insert into albums (album_name, artist_id, release_year) values ('Battle Born', 1, 2012)
-insert into songs (song_name, artist_id, album_id) values ('Flesh and Bone',1, 1)
-insert into songs (song_name, artist_id, album_id) values ('Runaways',1, 1)
-insert into songs (song_name, artist_id, album_id) values ('The Way It Was',1, 1)
-insert into songs (song_name, artist_id, album_id) values ('Here With Me',1, 1)
-insert into songs (song_name, artist_id, album_id) values ('A Matter Of Time',1, 1)
-insert into songs (song_name, artist_id, album_id) values ('Deadlines And Commitments',1, 1)
-insert into songs (song_name, artist_id, album_id) values ('Miss Atomic Bomb',1, 1)
-insert into songs (song_name, artist_id, album_id) values ('The Rising Tide',1, 1)
-insert into songs (song_name, artist_id, album_id) values ('Heart of a Girl',1, 1)
-insert into songs (song_name, artist_id, album_id) values ('From Here On Out',1, 1)
-insert into songs (song_name, artist_id, album_id) values ('Be Still',1, 1)
-insert into songs (song_name, artist_id, album_id) values ('Battle Born',1, 1)
+insert into albums (album_name, artist_id, release_year) values ('Battle Born', 1, 2012);
+insert into songs (song_name, artist_id, album_id) values ('Flesh and Bone',1, 1);
+insert into songs (song_name, artist_id, album_id) values ('Runaways',1, 1);
+insert into songs (song_name, artist_id, album_id) values ('The Way It Was',1, 1);
+insert into songs (song_name, artist_id, album_id) values ('Here With Me',1, 1);
+insert into songs (song_name, artist_id, album_id) values ('A Matter Of Time',1, 1);
+insert into songs (song_name, artist_id, album_id) values ('Deadlines And Commitments',1, 1);
+insert into songs (song_name, artist_id, album_id) values ('Miss Atomic Bomb',1, 1);
+insert into songs (song_name, artist_id, album_id) values ('The Rising Tide',1, 1);
+insert into songs (song_name, artist_id, album_id) values ('Heart of a Girl',1, 1);
+insert into songs (song_name, artist_id, album_id) values ('From Here On Out',1, 1);
+insert into songs (song_name, artist_id, album_id) values ('Be Still',1, 1);
+insert into songs (song_name, artist_id, album_id) values ('Battle Born',1, 1);
+
