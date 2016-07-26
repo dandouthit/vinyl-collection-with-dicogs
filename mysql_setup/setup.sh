7a6b984c4014f40e2dfe36f46eb1cbcf37466a25
Dan Douthit <danieldouthit@gmail.com>
2016-07-25 22:00:47 -0600

    added sleep for 10 seconds to let environment variables set up correctly before proceeding

--- mysql_setup/setup.sh
+++ mysql_setup/setup.sh
@@ -3,16 +3,14 @@
 # Run the MySQL container, with a database named 'records' and credentials
 # for a records-service user which can access it.
 echo "Starting DB..."  
-docker run --name db -d \  
-  -e MYSQL_ROOT_PASSWORD=123 \
-  -e MYSQL_DATABASE=records -e MYSQL_USER=records_service -e MYSQL_PASSWORD=123 \
-  -p 3306:3306 \
-  mysql:latest
+docker run --name db -d -e MYSQL_ROOT_PASSWORD=123 -e MYSQL_DATABASE=records -e MYSQL_USER=records_service -e MYSQL_PASSWORD=123 -p 3306:3306 mysql:latest
+
+sleep 10
 
 # Wait for the database service to start up.
 echo "Waiting for DB to start up..."  
-docker exec db mysqladmin --silent --wait=30 -urecords_service -p123 ping || exit 1
+docker exec db mysqladmin --silent --wait=30 -uroot -p123 ping || exit 1
 
 # Run the setup script.
 echo "Setting up initial data..."  
-docker exec -i db mysql -urecords_service -p123 records < records_create.sql  
+docker exec -i db mysql -uroot -p123 records < records_create.sql  
