efcee77827350b4f44dd8a3b744e36d87d21bb9b
Dan Douthit <danieldouthit@gmail.com>
2016-09-18 21:01:37 -0600

    adding new files for apache as the web server

---
+++ apache_setup/apache_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Run the apace container and copy a simple homepage to the default location and expose port 80
+echo "Starting apache..."
+docker run --name apache -v $PWD/content:/usr/local/apache2/htdocs/:ro -d -p 8080:80 httpd:2.4
+
